ab61ab39c0a58cb136681718108c077bf7ec8510
Isma007 <166597813+Isma007@users.noreply.github.com>
2024-04-11 12:10:23 +0200

    Add files via upload

---
+++ net_scanner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#Network_Scaner
+import scapy.all as scapy
+
+
+
+def scan(ip):
+    arp_request=scapy.ARP(pdst=ip)
+
+    broadcast=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    arp_request_broadcast=broadcast/arp_request
+
+    answered_list=scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    clients_list=[]
+
+    for element in answered_list:
+        zero = {"ip":element[1].psrc,"mac":element[1].hwsrc}
+        clients_list.append(zero)
+
+    return clients_list
+
+def print_result(results_list):
+    print("IP\t\t\t","MAC Address\n-------------------------------------")
+    for element_client in results_list:
+        print(element_client["ip"]+"\t\t"+element_client["mac"])
+
+scanResult=scan("192.168.58.1/24")
+
+print_result(scanResult)
